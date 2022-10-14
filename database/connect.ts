a5ff0c30aa978602bad85c701c28fbf29def8615
Rhea Dieberger <rdieberger@gmail.com>
2022-10-14 13:18:41 +0200

    Adapt database connect file

--- database/connect.ts
+++ database/connect.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-if (!process.env.FLY_IO_BUILD) config();
+if (process.env.NODE_ENV !== 'production') config();
 
 // Type needed for the connection function below
 declare module globalThis {
