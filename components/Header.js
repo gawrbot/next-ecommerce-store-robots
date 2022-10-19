45f87596b6383041f08b5767619b05c47fe49ec4
Rhea Dieberger <rdieberger@gmail.com>
2022-10-19 20:46:47 +0200

    Put icon and cart count in same Link el

--- components/Header.js
+++ components/Header.js
@@ -25,12 +25,10 @@ export default function Header(props) {
           </a>
         </Link>
         <Link href="/shopping-cart">
-          <div className="absolute right-12 top-5 w-10 h-10 hover:cursor-pointer">
-            <img alt="robot icon" src="/robot (1).png" />
-          </div>
-        </Link>
-        <Link href="/shopping-cart">
           <a data-test-id="cart-link">
+            <div className="absolute right-12 top-5 w-10 h-10 hover:cursor-pointer">
+              <img alt="robot icon" src="/robot (1).png" />
+            </div>
             <div className="absolute right-9 top-9 flex items-center justify-center rounded-full px-2 py-2 bg-pink-600/[.85] text-white hover:cursor-pointer w-8 h-8">
               <span data-test-id="cart-count">
                 {robotsInCart > 0 ? robotsInCart : 0}
