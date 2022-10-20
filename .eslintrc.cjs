6357bebdf121406c98587c03829eb17c54d58f70
Rhea Dieberger <rdieberger@gmail.com>
2022-10-20 16:46:56 +0200

    Install ESLint 3.0.1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,3 @@
 module.exports = {
   extends: ['@upleveled/upleveled'],
-  // plugins: ['@ts-safeql/eslint-plugin'],
-  // rules: {
-  //   '@ts-safeql/check-sql': [
-  //     'error',
-  //     {
-  //       connections: [
-  //         {
-  //           databaseUrl: 'postgres://${process.env.PGUSERNAME}:${process.env.PGPASSWORD}@${process.env.PGHOST}:5432/${process.env.PGDATABASE}',
-  //           tagName: 'sql',
-  //           fieldTransform: 'camel',
-  //           transform: '{type}[]',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
 };
